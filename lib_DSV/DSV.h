3ae633a7367cfb9f48b0190a44e9b78a62298fc5
alina <AlinaSedyhan@gmail.com>
2025-11-11 15:22:03 +0300

    added realization of DSV with test

---
+++ lib_DSV/DSV.h
@@ -0,0 +1,106 @@
+class DSV {
+    int* _parent;
+    int _size;
+    int* _rank;
+public:
+    DSV(int size);
+    DSV(const DSV& other); 
+    DSV& operator=(const DSV& other); 
+    void unite(int x1, int x2);
+    int find(int x);
+    bool connected(int x1, int x2);
+    int size() const;
+    ~DSV();
+};
+
+DSV::DSV(int size) : _size(size)
+{
+    _parent = new int[_size];
+    _rank = new int[_size];
+    for (int i = 0; i < _size; i++)
+    {
+        _parent[i] = i;
+        _rank[i] = 0;
+    }
+}
+
+DSV::DSV(const DSV& other) : _size(other._size)
+{
+    _parent = new int[_size];
+    _rank = new int[_size];
+    for (int i = 0; i < _size; i++)
+    {
+        _parent[i] = other._parent[i];
+        _rank[i] = other._rank[i];
+    }
+}
+
+DSV& DSV::operator=(const DSV& other)
+{
+    if (this != &other) 
+    {
+        delete[] _parent;
+        delete[] _rank;
+
+        _size = other._size;
+        _parent = new int[_size];
+        _rank = new int[_size];
+
+        for (int i = 0; i < _size; i++)
+        {
+            _parent[i] = other._parent[i];
+            _rank[i] = other._rank[i];
+        }
+    }
+    return *this;
+}
+
+void DSV::unite(int x1, int x2)
+{
+    int root1 = find(x1);
+    int root2 = find(x2);
+
+    if (root1 == root2) 
+    {
+        return;
+
+    }
+    if (_rank[root1] < _rank[root2])
+    {
+        _parent[root1] = root2;
+    }
+    else if (_rank[root1] > _rank[root2])
+    {
+        _parent[root2] = root1;
+    }
+    else
+    {
+        _parent[root2] = root1;
+        _rank[root1]++;
+    }
+}
+
+int DSV::find(int x)
+{
+    if (_parent[x] != x)
+    {
+        _parent[x] = find(_parent[x]); 
+    }
+    return _parent[x];
+}
+
+bool DSV::connected(int x1, int x2)
+{
+    return find(x1) == find(x2);
+}
+
+int DSV::size() const
+{
+    return _size;
+}
+
+DSV::~DSV()
+{
+    delete[] _parent;
+    delete[] _rank;
+}
---
+++ tests/test_DSV.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <cassert>
+#include <gtest/gtest.h>
+#include "../lib_DSV/DSV.h"
+
+TEST(DSVTest, can_create_dsv) 
+{
+    ASSERT_NO_THROW(DSV dsv(10));
+}
+
+TEST(DSVTest, can_find_initial_elements) 
+{
+
+    DSV dsv(5);
+    for (int i = 0; i < 5; i++) 
+    {
+        ASSERT_NO_THROW(dsv.find(i));
+    }
+}
+
+TEST(DSVTest, can_unite_elements) 
+{
+    DSV dsv(5);
+    ASSERT_NO_THROW(dsv.unite(0, 1));
+    ASSERT_NO_THROW(dsv.unite(2, 3));
+}
+
+TEST(DSVTest, can_check_connections)
+{
+    DSV dsv(5);
+    dsv.unite(0, 1); 
+    ASSERT_NO_THROW(dsv.connected(0, 1));
+    ASSERT_NO_THROW(dsv.connected(0, 2));
+}
+
+TEST(DSVTest, can_copy_dsv)
+{
+    DSV dsv1(5);
+    dsv1.unite(0, 1);
+    ASSERT_NO_THROW(DSV dsv2 = dsv1);
+}
+
+TEST(DSVTest, can_assign_dsv)
+{
+    DSV dsv1(5);
+    DSV dsv2(3);
+    dsv1.unite(0, 1);
+    ASSERT_NO_THROW(dsv2 = dsv1);
+}
+
+TEST(DSVTest, can_unite_multiple_elements) 
+{
+    DSV dsv(10);
+    ASSERT_NO_THROW(dsv.unite(0, 1));
+    ASSERT_NO_THROW(dsv.unite(1, 2));
+    ASSERT_NO_THROW(dsv.unite(3, 4));
+    ASSERT_NO_THROW(dsv.unite(2, 3));
+}
+
+TEST(DSVTest, can_handle_self_union)
+{
+    DSV dsv(5);
+    ASSERT_NO_THROW(dsv.unite(2, 2));
+}
+
+TEST(DSVTest, can_create_large_dsv) 
+{
+    ASSERT_NO_THROW(DSV dsv(1000));
+}
+
+TEST(DSVTest, can_use_after_copy)
+{
+    DSV dsv1(5);
+    dsv1.unite(0, 1);
+    DSV dsv2 = dsv1;
+    ASSERT_NO_THROW(DSV dsv2 = dsv1);
+    ASSERT_NO_THROW(dsv2.find(0));
+    ASSERT_NO_THROW(dsv2.connected(0, 1));
+}
